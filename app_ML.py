f812af2b19c36f6097199f4c6f8262ebdd210383
Rezoneter <150760616+Rezoneter@users.noreply.github.com>
2023-11-22 15:12:54 +0900

    build predict model

--- app_ML.py
+++ app_ML.py
@@ -1,4 +1,37 @@
 import streamlit as st
+import joblib
+import numpy as np
+
+
 
 def run_ML_app():
     st.subheader('Purchase price predict')
+
+    # learning file read,
+    # develope predict screen
+
+    regressor = joblib.load('./model/regressor.pkl')
+
+    gender = st.radio('Select sex',['Female', 'Male'])
+
+    if gender == 'Female':
+        gender = 0
+    else :
+        gender = 1 
+
+    age = st.number_input('Input Age', 20, 100)
+
+    salary = st.number_input ('Input Salary', 10000, 1000000)
+
+    debt = st.number_input('Input Card debt', 0, 100000)
+
+    worth = st.number_input('Input Worth', 1000, 1000000)
+
+    if st.button('Predict purcahse price'):
+        selected_data = np.array([gender, age, salary, debt, worth])
+        selected_data = selected_data.reshape(1,5)
+        y_pred = regressor.predict(selected_data)
+        st.text(y_pred)
+    else:
+        pass
+    
